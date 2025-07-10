cd0b2f5091121e9b7aa94ea4f65ff96dfc4e8a05
metabolgia <onicolas.coral@gmail.com>
2025-07-09 21:12:32 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,22 @@ import ReactDOM from "react-dom/client"
 import RecetarioAguaCafe from "./RecetarioAguaCafe"
 import "./index.css"
 
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RecetarioAguaCafe />
   </React.StrictMode>
 )
+
+function App() {
+  useEffect(() => {
+    // Este código se ejecuta una sola vez cuando se monta la app
+    const splash = document.getElementById("splash")
+    if (splash) splash.remove() // Elimina el div que bloquea la pantalla
+  }, []) // El array vacío [] significa "solo una vez al montar"
+
+  return <RecetarioAguaCafe />
+}
+
+export default App
